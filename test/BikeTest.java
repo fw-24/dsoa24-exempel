dd5383b59fbb1769dd5a74ed151919520f358287
Fredrik Welander <welandfr@arcada.fi>
2024-01-15 11:34:41 +0200

    Mellancommit2

---
+++ test/BikeTest.java
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BikeTest {
+
+    @Test
+    void testInstantiateBike() {
+        Bike bike = new Bike("testbike");
+
+        assertEquals("testbike", bike.getName());
+    }
+
+    @Test
+    void testBikePrice() {
+        Bike bike = new Bike("testbike", 100);
+        assertEquals(100, bike.getPrice());
+    }
+
+}
