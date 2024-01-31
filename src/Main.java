9680cf9491d853bd52d87f46fe4fc7f04fa5d79f
Fredrik Welander <welandfr@arcada.fi>
2024-01-31 14:23:28 +0200

    Lektion 6 Tips för att swappa objekt

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,32 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Scanner strInput = new Scanner(System.in);
+
+        /**
+         * Tips för att swappa mellan två objekt
+         */
+        Vehicle bike = new Bike("cykel");
+        Vehicle car = new Car("bil");
+
+        Vehicle todaysRide = bike;
+        Vehicle tomorrowsRide = car;
+        Vehicle tempRide;
+
+        // Vi byter fordon varje dag
+        for (int i = 1; i < 7; i++) {
+            System.out.println("-- Dag " + i);
+            System.out.printf("I dag åker jag %s, imorgon åker jag %s\n",
+                    todaysRide.getName(),
+                    tomorrowsRide.getName());
+
+            // Själva swappen
+            tempRide = todaysRide;
+            todaysRide = tomorrowsRide;
+            tomorrowsRide = tempRide;
+
+        }
+        /* objekt-swapping end */
+
 
         BikeShop shop = new BikeShop();
         shop.addBike(new Bike("Scott", 500));
@@ -13,11 +38,11 @@ public class Main {
 
         System.out.println("Välj cykel att köpa:");
         for (int i = 0; i < shop.getBikes().size(); i++) {
-            Bike bike = shop.getBikes().get(i);
+            Bike b = shop.getBikes().get(i);
             System.out.printf("[%d] %s kostar %.2f\n",
                     i,
-                    bike.getName(),
-                    bike.getPrice());
+                    b.getName(),
+                    b.getPrice());
         }
 
         // Code Challenge: input
